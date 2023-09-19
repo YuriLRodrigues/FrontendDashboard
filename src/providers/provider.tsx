57dd9beb3801633b553923aedfbc00b421ea16fa
yurilrodrigues <rodriguesyuri769@gmail.com>
2023-09-18 21:03:40 -0300

    providers englobados

--- src/providers/provider.tsx
+++ src/providers/provider.tsx
@@ -4,13 +4,16 @@ import { ReactNode } from "react";
 import { ThemeProvider as NextThemeProvider } from "next-themes";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "@/client/queryClient";
+import { SidebarProvider } from "@/context/SidebarContext";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <NextThemeProvider>
-      <QueryClientProvider client={queryClient}>
-        <SessionProvider>{children}</SessionProvider>
-      </QueryClientProvider>
+      <SidebarProvider>
+        <QueryClientProvider client={queryClient}>
+          <SessionProvider>{children}</SessionProvider>
+        </QueryClientProvider>
+      </SidebarProvider>
     </NextThemeProvider>
   );
 }
