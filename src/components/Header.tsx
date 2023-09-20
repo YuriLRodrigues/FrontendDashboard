4e245dcc38b74b5a8687e134b6a9e140728a56c8
yurilrodrigues <rodriguesyuri769@gmail.com>
2023-09-20 13:48:29 -0300

    feat: componente header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,17 @@
 "use client";
 import { useSidebarContext } from "@/context/SidebarContext";
 import { useSession } from "next-auth/react";
-import { AiOutlinePlusCircle } from "react-icons/ai";
 import { RiMenuUnfoldLine } from "react-icons/ri";
 import ToggleTheme from "./Theme";
+import { TransactionModalButton } from "./TransactionModalButton";
+import { MdOutlineNotifications } from "react-icons/md";
 
 export default function Header() {
   const { data: session } = useSession();
   const { isOpenAside, setIsOpenAside } = useSidebarContext();
 
   return (
-    <header className="duration-500 w-fulll flex items-center dark:bg-zinc-700 bg-zinc-100 h-14 justify-between px-2">
+    <header className="duration-500 w-fulll flex items-center   h-14 justify-between px-2">
       <button
         className="text-3xl md:hidden sticky"
         onClick={() => setIsOpenAside(!isOpenAside)}
@@ -20,7 +21,7 @@ export default function Header() {
       <div className="md:flex hidden gap-2">
         {session?.user.name && (
           <div className="md:flex gap-2 items-center ml-4">
-            <span className="uppercase p-1  rounded-md text-center bg-purple-600 dark:text-white">
+            <span className="uppercase border w-[30px] h-[30px] rounded-full flex items-center justify-center bg-purple-600 text-white border-purple-600">
               {session.user.name.substring(0, 2)}
             </span>
 
@@ -28,11 +29,12 @@ export default function Header() {
           </div>
         )}
       </div>
+      <div className="flex gap-4 items-center">
+
+      <span className="text-xl font-semibold rounded-full cursor-pointer p-1 hover:bg-zinc-400/10"><MdOutlineNotifications /></span>
         <ToggleTheme />
-      <button className="flex items-center gap-2 dark:border-2 rounded p-1 dark:bg-white dark:text-purple-600 bg-purple-600 text-white">
-        {" "}
-        {<AiOutlinePlusCircle />} Nova transação
-      </button>
+        <TransactionModalButton />
+      </div>
     </header>
   );
 }
