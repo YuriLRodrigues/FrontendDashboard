666dd698db08183195fb730dce6f678552982e04
yurilrodrigues <rodriguesyuri769@gmail.com>
2023-09-20 13:48:13 -0300

    feat: componente dos dashs

--- src/components/AreaChart.tsx
+++ src/components/AreaChart.tsx
@@ -1,37 +1,78 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth-options";
 import { AreaChart, Card, Title } from "@tremor/react";
 import { getServerSession } from "next-auth";
 
-export default async function FinancesDash() {
-  const session = await getServerSession(authOptions)
+type FinanceDatasType = {
+  transation: string,
+  day: string,
+  hour: string,
+  minutes: string,
+  month: string,
+  year: string,
+  value: number,
+  id: string,
+  title: string,
+  store: string | null,
+  payment: string |  null,
+  product: string | null
+}
 
-  try {
-    const res = await fetch("http://localhost:3000/api/get", {
-      method: "POST",
+type FinanceType = {
+  FinanceData: FinanceDatasType[]
+}
+
+type Date = {
+  date: string,
+  title: string,
+  valor: number
+}
+
+export const revalidate = 1000
+
+export default async function FinancesDash() {
+  const session = await getServerSession(authOptions);
+
+  const userData = await fetch(
+    "https://backend-dashboard-opal.vercel.app/myextracts",
+    {
+      
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${session?.user.token}`,
       },
-      body: JSON.stringify(session?.user.token)
-    }).then((data)=> data.json())
-    
-    console.log(res)
-  } catch (error) {
-    console.log(error)
-  }
+    }
+  ).then(async (res) => await res.json());
+  
+  console.log(userData)
+  const finances: FinanceDatasType[] = userData.map((finance:FinanceType)=> finance.FinanceData)
+  const data:Date[] = []
+  
+  finances.map((fin)=> {
+    if (fin.transation === "entrada"){
 
+      data.push({
+        date: `${fin.day} ${fin.month} ${fin.year}`,
+        title: fin.title,
+        valor: fin.value
+      })
+    }
+  })
+  console.log(data)
+  
   return (
     <div className="grid p-4">
-      {/* <Card>
-        <Title>Newsletter revenue over time (USD)</Title>
+      <Card>
+        <Title>Total valores de entradas</Title>
         <AreaChart
           className="h-72 mt-4"
-          data={[1,2,3,4,5]}
+          data={data}
           index="date"
-          categories={["SemiAnalysis", "The Pragmatic Engineer"]}
+          categories={["valor"]}
           colors={["indigo", "cyan"]}
           // valueFormatter={dataFormatter}
         />
-      </Card> */}
+      </Card>
     </div>
   );
 }
