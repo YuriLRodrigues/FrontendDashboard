a77c457bfdc0ee5650cea517ae84417440420320
yurilrodrigues <rodriguesyuri769@gmail.com>
2023-09-20 13:49:02 -0300

    feat: componente theme

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,18 +1,15 @@
-
+"use client"
 import { useTheme } from "next-themes";
-import { BsMoonStarsFill, BsFillSunFill  } from "react-icons/bs";
-import { GrSystem } from "react-icons/gr";
+import { BsMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 
 export default function ToggleTheme() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className="text-center text-xl">
-      
-        <button onClick={()=> setTheme(theme === 'light' ? "dark" : theme === "dark" ? "light" : "light")} className="cursor-pointer text-purple-600">
-          {theme === 'light' ? <BsFillSunFill /> : theme === "dark" ? <BsMoonStarsFill /> : <GrSystem />}
-        </button>
-      
-    </div>
+    <button
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      {theme === "light" ? <BsFillSunFill /> : <BsMoonStarsFill />}
+    </button>
   );
 }
