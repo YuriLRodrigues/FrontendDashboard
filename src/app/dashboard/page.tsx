4fe2b9dcd7ec79aca3e1881ace0dd704cd1bd8aa
YuriLRodrigues <rodriguesyuri769@gmail.com>
2023-09-25 22:18:27 -0300

    feat:feat: adicionando a

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,27 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import FinancesDash from "@/components/AreaChart";
+import FinancesDash from "@/components/FinancesDash";
 import { authOptions } from "@/lib/auth-options";
+import NotUserData from "@/components/NotUserData";
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/");
+  
+  const userData = await fetch(
+    "https://backend-dashboard-gold.vercel.app/myextracts",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${session?.user.token}`,
+      },
+    }
+  ).then(async (res) => await res.json());
 
-  return <FinancesDash />;
+  if (userData.length > 0) {
+    return <FinancesDash />;
+  }
 
+  return <NotUserData />
 }
